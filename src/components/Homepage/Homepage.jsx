6f3a7321dba60d21666400916079ee91500544d4
Techxie-bc <donnaogbu@gmail.com>
2023-07-26 01:16:51 -0700

    animations were added to the components

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,5 +1,7 @@
 import Navbar from "../Navbar/Navbar";
 import './homepage.css';
+import Fade from 'react-reveal/Fade';
+import RubberBand from 'react-reveal/RubberBand';
 import dev from '../../Images/undraw_teacher_re_sico.svg'
 import { ReactComponent as Reactic } from '../../Images/react-js-icon.svg'
 import { ReactComponent as Javasc } from '../../Images/javascript-programming-language-icon.svg'
@@ -25,6 +27,7 @@ export default function Homepage() {
             <div className="container">
 
                 <div className="home">
+                <Fade left>
                     <div className="bio">
                         <div className="topbio">
                             <p className="shake-emoji">&#x1F44B; Warm Greetings from Irene</p>
@@ -36,15 +39,20 @@ export default function Homepage() {
                             </h3>
                         </div>
                     </div>
+                    </Fade>
+                    <Fade right>
                     <div className="imagehome">
                         <img src={dev} alt="Frontend developer" />
                     </div>
+                    </Fade>
                 </div>
                 {/* TECHNOLOGIES */}
                 <div className="tech-body">
                     <div className="list">
                         <div class="button-wrap">
+                        <RubberBand>
                             <button class="tech-title">TECHNOLOGIES</button>
+                        </RubberBand>
                         </div>
                         <div className="tools">
                             <div className="tool">
@@ -73,19 +81,25 @@ export default function Homepage() {
 
                 </div>
                 <div className="bio">
-                    <button className="biotitle">
-                        <h3>WORK ETHICS </h3>
-                    </button>
-                    <h5>
-                        "Smart Frontend Engineer. I help startups launch and grow, and enable companies to boost their profits. As a valuable asset to your team day and night, I'm readily available to make your website dreams come true.
+                    <Fade bottom>
+                        <button className="biotitle">
+                            <h3>WORK ETHICS </h3>
+                        </button>
+                    </Fade>
+                    <Fade bottom>
 
-                        I pride myself on learning technologies fast, allowing me to stay at the forefront of cutting-edge developments in the frontend landscape. Working seamlessly with cross-functional teams in time-pressured settings, I ensure projects are completed on schedule and within budget. <br />
-                        As a frontend engineer, I wholeheartedly embrace the growth mindset. I see challenges as stepping stones to progress and approach every obstacle with a thirst for learning. Embracing this mindset empowers me to continually evolve, staying on the cutting edge of the rapidly evolving web technologies. With a growth-oriented perspective, I fearlessly take on ambitious projects, pushing the boundaries of my skills and expertise.<br />
+                        <h5>
+                            "Smart Frontend Engineer. I help startups launch and grow, and enable companies to boost their profits. As a valuable asset to your team day and night, I'm readily available to make your website dreams come true.
 
-                        The growth mindset fuels my passion for excellence, enabling me to thrive in a dynamic and ever-changing digital landscape. I relish in the satisfaction of conquering new coding techniques and honing my craft, always seeking opportunities to expand my knowledge. By nurturing this mindset, I'm not just a frontend engineer; I'm a lifelong learner on an exciting journey of personal and professional growth.<br />
+                            I pride myself on learning technologies fast, allowing me to stay at the forefront of cutting-edge developments in the frontend landscape. Working seamlessly with cross-functional teams in time-pressured settings, I ensure projects are completed on schedule and within budget. <br />
+                            As a frontend engineer, I wholeheartedly embrace the growth mindset. I see challenges as stepping stones to progress and approach every obstacle with a thirst for learning. Embracing this mindset empowers me to continually evolve, staying on the cutting edge of the rapidly evolving web technologies. With a growth-oriented perspective, I fearlessly take on ambitious projects, pushing the boundaries of my skills and expertise.<br />
 
-                        Let's collaborate and bring your digital vision to life with a touch of creativity and seamless user experiences. Together, we'll make a lasting impact on the digital world. 🚀"
-                    </h5>
+                            The growth mindset fuels my passion for excellence, enabling me to thrive in a dynamic and ever-changing digital landscape. I relish in the satisfaction of conquering new coding techniques and honing my craft, always seeking opportunities to expand my knowledge. By nurturing this mindset, I'm not just a frontend engineer; I'm a lifelong learner on an exciting journey of personal and professional growth.<br />
+
+                            Let's collaborate and bring your digital vision to life with a touch of creativity and seamless user experiences. Together, we'll make a lasting impact on the digital world. 🚀"
+                        </h5>
+                    </Fade>
+
                 </div>
                 <div className="projects">
                     <div className="pro-body">
@@ -95,6 +109,7 @@ export default function Homepage() {
                             </button>
                         </div>
                         <div className="projectbody">
+                            <Fade right>
                             <div className="projectlist">
                                 <div className="project">
                                     <img src={loan} alt="coin island" />
@@ -149,47 +164,48 @@ export default function Homepage() {
                                     </div>
                                 </div>
                             </div>
+                            </Fade>
                         </div>
                     </div>
                 </div>
                 <div className="contact-body">
                     <div className="contacts">
                         <a href="https://github.com/Techxie-bc" target="_blank" rel="noreferrer">
-                        <div className="github">
-                        <img src={github} alt="github" />
-                            <h3>GitHub</h3>
-                        </div>
+                            <div className="github">
+                                <img src={github} alt="github" />
+                                <h3>GitHub</h3>
+                            </div>
                         </a>
-                            <a href="mailto:donnaogbu@gmail.com" target="_blank" rel="noreferrer">
-                        <div className="mail">
-                        <img src={mail} alt="mail" />
-                            <h3>Mail</h3>
-                        </div>
+                        <a href="mailto:donnaogbu@gmail.com" target="_blank" rel="noreferrer">
+                            <div className="mail">
+                                <img src={mail} alt="mail" />
+                                <h3>Mail</h3>
+                            </div>
                         </a>
-                            <a href="/">
-                        <div className="twitter">
-                        <img src={twitter} alt="twitter" />
-                            <h3>Twitter</h3>
-                        </div>
+                        <a href="/">
+                            <div className="twitter">
+                                <img src={twitter} alt="twitter" target="_blank" rel="noreferrer" />
+                                <h3>Twitter</h3>
+                            </div>
                         </a>
-                            <a href="www.linkedin.com/in/irene-onyia-522783176" target="_blank" rel="noreferrer" >
-                        <div className="linkedin">
+                        <a href="www.linkedin.com/in/irene-onyia-522783176">
+                            <div className="linkedin">
                                 <img src={linkedin} alt="linkedin" />
-                            <h3>LinkedIn</h3>
-                        </div>
+                                <h3>LinkedIn</h3>
+                            </div>
                         </a>
-                        <a href="https://wa.me/+2348133491087" target="_blank" rel="noreferrer" >
-                        <div className="whatsapp">
-                            <IoLogoWhatsapp size={38}/>
-                             <h3>Whatsapp</h3>
-                         </div>
-                         </a>
+                        <a href="https://wa.me/+2348133491087">
+                            <div className="whatsapp">
+                                <IoLogoWhatsapp size={38} />
+                                <h3>Whatsapp</h3>
+                            </div>
+                        </a>
                     </div>
                 </div>
                 <div className="footer">
                     <p>Made with love <span>💚</span> &copy; Copyright 2023, Chiamaka Irene Onyia. All Rights Reserved.</p>
                 </div>
-                
+
             </div>
 
         </>
--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,135 +1,139 @@
 import './resume.css'
 import '../Homepage/homepage.css'
+import Fade from 'react-reveal/Fade';
 import Navbar from '../Navbar/Navbar'
 
-export default function Resume(){
+export default function Resume() {
     return (
         <>
-        <Navbar/>
+            <Navbar />
             <div className="resumebody">
-            <div class="button-wrap">
-                                <button class="tech-title">RESUME</button>
-                            </div>
+                <div class="button-wrap">
+                    <button class="tech-title">RESUME</button>
+                </div>
                 <div className="resume">
-                    <h1 className='prof' >
-                    Chiamaka Irene Onyia
-donnaogbu@gmail.com +2348133491087 Imo, Nigeria.</h1>
-<h4>
-A results-driven Front-End Developer, specializing in UI design and web development. A proven track
-record of collaborating cross-functionally with technical teams and stakeholders across all phases of
-the software development lifecycle.
+                    <Fade bottom>
+                        <h1 className='prof' >
+                            Chiamaka Irene Onyia
+                            donnaogbu@gmail.com +2348133491087 Imo, Nigeria.</h1>
+                        <h4>
+                            A results-driven Front-End Developer, specializing in UI design and web development. A proven track
+                            record of collaborating cross-functionally with technical teams and stakeholders across all phases of
+                            the software development lifecycle.
 
-</h4>
-    
-<h3 className='prof'>Professional Experience</h3>
-<h3 className='experience'>
+                        </h4>
 
-Front-End Developer
-Balancee Tech solutions. Lagos, Nigeria. September 2022 — Present
-</h3>
-<div>
-    <ul>
-        <li>
-        Designed the user interface and website www.balancee.app for the Balancee tech services
-delivering web development services for a company valued at $100k
-        </li>
-        <li>
-        Translate user requirements into scalable code, develop prototypes conduct wireframing
-activities, and ensure alignment with client business specifications
-        </li>
-        <li>
-        Perform testing on web pages, analyze user feedback, and coordinate with cross-
-functional teams to identify opportunities to enhance web page responsiveness.
-        </li>
-        <li>
-        Partnered with web developers and software developers to create a dynamic web
-application for autoshops and customers, which included supporting back-end
-development andUI design
-        </li>
-    </ul>
-</div>
-<br></br>
-<hr/>
-<h3 className="prof">Education</h3>
-<div>
-    <ul>
-        <li>
+                        <h3 className='prof'>Professional Experience</h3>
+                        <h3 className='experience'>
 
-Bachelor of Technology (B.Tech.)Dental Technology
-<br/> 
-Federal University of Technology, Owerri, Imo, Nigeria November 2011 - October 2016
-        </li>
-        <li>
-Diploma Software Engineering,
-<br/>
-ALX Africa
+                            Front-End Developer
+                            Balancee Tech solutions. Lagos, Nigeria. September 2022 — Present
+                        </h3>
+                        <div>
+                            <ul>
+                                <li>
+                                    Designed the user interface and website www.balancee.app for the Balancee tech services
+                                    delivering web development services for a company valued at $100k
+                                </li>
+                                <li>
+                                    Translate user requirements into scalable code, develop prototypes conduct wireframing
+                                    activities, and ensure alignment with client business specifications
+                                </li>
+                                <li>
+                                    Perform testing on web pages, analyze user feedback, and coordinate with cross-
+                                    functional teams to identify opportunities to enhance web page responsiveness.
+                                </li>
+                                <li>
+                                    Partnered with web developers and software developers to create a dynamic web
+                                    application for autoshops and customers, which included supporting back-end
+                                    development andUI design
+                                </li>
+                            </ul>
+                        </div>
+                        <br></br>
+                        <hr />
+                        <h3 className="prof">Education</h3>
+                        <div>
+                            <ul>
+                                <li>
 
-        </li>
-        <li>
-Frontend Engineering Course,
-<br/>
+                                    Bachelor of Technology (B.Tech.)Dental Technology
+                                    <br />
+                                    Federal University of Technology, Owerri, Imo, Nigeria November 2011 - October 2016
+                                </li>
+                                <li>
+                                    Diploma Software Engineering,
+                                    <br />
+                                    ALX Africa
 
-Stutern Graduate Accelerator
-        </li>
-    </ul>
-</div>
+                                </li>
+                                <li>
+                                    Frontend Engineering Course,
+                                    <br />
 
-<br/>
-<hr/>
+                                    Stutern Graduate Accelerator
+                                </li>
+                            </ul>
+                        </div>
 
-<h3 className="prof">
+                        <br />
+                        <hr />
 
-Key Skills
-</h3>
-<div>
-    <ul>
-        <li>
+                        <h3 className="prof">
 
- Reactjs and Redux state Management
-        </li>
-        <li>
+                            Key Skills
+                        </h3>
+                        <div>
+                            <ul>
+                                <li>
 
-Javascript and Dom manipulation
-        </li>
-        <li>
+                                    Reactjs and Redux state Management
+                                </li>
+                                <li>
 
- Git version control
-        </li>
-        
-        <li>
+                                    Javascript and Dom manipulation
+                                </li>
+                                <li>
 
-CSS, Styled components, TailwindCSS, bootstrap
-        </li>
-        <li>
+                                    Git version control
+                                </li>
 
-Html
-        </li>
-    </ul>
-</div>
+                                <li>
 
-<br/>
-<hr/>
+                                    CSS, Styled components, TailwindCSS, bootstrap
+                                </li>
+                                <li>
 
-<h2 className='prof'>
+                                    Html
+                                </li>
+                            </ul>
+                        </div>
 
-Personal Projects
-</h2>
-<div>
-    <ul>
-        <li>
-        <a href="https://coinschool.netlify.app/ ">Coinschool</a> Sample of an online web3 school website built with React
-    Framework, hooks and styled with styled components.
-        </li>
-        <li>
-        <a href="https://coinislander.netlify.app">CoinIslander</a> Built with the React Library, coinislander gives live feed on
-    CryptoCurrencies.
-        </li>
-        <li>
-        <a href="https://frankflix-project.web.app">Frankflix</a> Landing Page of a movie site designed with Reactjs. Firebase
-    Authentication was implemented for users' login details and Firestore for cloud database.
-        </li>
-    </ul>
-</div>
+                        <br />
+                        <hr />
+
+                        <h2 className='prof'>
+
+                            Personal Projects
+                        </h2>
+                        <div>
+                            <ul>
+                                <li>
+                                    <a href="https://coinschool.netlify.app/ ">Coinschool</a> Sample of an online web3 school website built with React
+                                    Framework, hooks and styled with styled components.
+                                </li>
+                                <li>
+                                    <a href="https://coinislander.netlify.app">CoinIslander</a> Built with the React Library, coinislander gives live feed on
+                                    CryptoCurrencies.
+                                </li>
+                                <li>
+                                    <a href="https://frankflix-project.web.app">Frankflix</a> Landing Page of a movie site designed with Reactjs. Firebase
+                                    Authentication was implemented for users' login details and Firestore for cloud database.
+                                </li>
+                            </ul>
+                        </div>
+
+                    </Fade >
 
                 </div>
                 <div className="download">
